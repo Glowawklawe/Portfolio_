813d43cea251db13e11843203fc2f1fc770ee69d
kamil Gruszfeld <kamilo_kamilo@o2.pl>
2024-10-14 18:37:04 +0000

    Build sript Navbar Active on scroll effect

--- js/script.js
+++ js/script.js
@@ -2,18 +2,23 @@
 
 // scroll sections
 let sections = document.querySelectorAll('section');
-let navLink = document.querySelectorAll('header nav a');
+let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
     sections.forEach(sec =>{
         let top = window.scrollY;
-        let offset = sec.offsetTop -100;
+        let offset = sec.offsetTop - 100;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if (top >= offset && top < offset + height)
+        if (top >= offset && top < offset + height) {
             // active navbar links
-    })
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
     // sticky header
     let header = document.querySelector('header');
 
